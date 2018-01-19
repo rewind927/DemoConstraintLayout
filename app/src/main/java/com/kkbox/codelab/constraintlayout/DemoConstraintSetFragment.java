77ab69fd6b736defc82e63d0cf819f9953a305bc
HanChen <hanchen@kkbox.com>
2018-01-19 18:12:21 +0800

    Using constraint set to imeplement basic animation

--- app/src/main/java/com/kkbox/codelab/constraintlayout/DemoConstraintSetFragment.java
+++ app/src/main/java/com/kkbox/codelab/constraintlayout/DemoConstraintSetFragment.java
@@ -3,15 +3,20 @@ package com.kkbox.codelab.constraintlayout;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
 import android.support.v4.app.Fragment;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class DemoConstraintSetFragment extends Fragment {
     ConstraintLayout rootView;
+    ConstraintSet originalConstraintSet = new ConstraintSet();
+    ConstraintSet fullscreenConstrainSet = new ConstraintSet();
+    boolean isFullscreen = false;
 
     public DemoConstraintSetFragment() {
         // Required empty public constructor
@@ -24,7 +29,7 @@ public class DemoConstraintSetFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        fullscreenConstrainSet.clone(getActivity(), R.layout.fragment_demo_constraint_set_fullscreen);
     }
 
     @Override
@@ -36,10 +41,27 @@ public class DemoConstraintSetFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.view_face).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(rootView);
+                if (isFullscreen) {
+                    originalConstraintSet.clone(getActivity(), R.layout.fragment_demo_constraint_set);
+                    originalConstraintSet.applyTo(rootView);
+                } else {
+                    fullscreenConstrainSet.applyTo(rootView);
+                }
+                isFullscreen = !isFullscreen;
+            }
+        });
+    }
+
+    @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        ConstraintSet constraintSet = new ConstraintSet();
-        constraintSet.clone(getActivity(), R.layout.fragment_demo_constraint_set);
-        constraintSet.applyTo(rootView);
+        originalConstraintSet.clone(getActivity(), R.layout.fragment_demo_constraint_set);
+        originalConstraintSet.applyTo(rootView);
     }
 }
